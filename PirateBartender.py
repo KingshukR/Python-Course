50301eaf4bd951d11911f963552ea86d7c8a0917
KingshukR <kingshukroy98@gmail.com>
2016-08-04 01:10:28 -0500

    Initial commit

---
+++ PirateBartender.py
@@ -0,0 +1,44 @@
+import random
+import sys 
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin",],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon",],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel",],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola",],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top",]
+}
+
+def customer_preferences():
+    preferences = {}
+    for type, question in questions.items():
+        print(question)
+        preferences[type] = input().lower() in ["y", "yes"]
+    return preferences
+
+def make_order(preferences):
+    order = []
+    for ingredient_type, liked in preferences.items():
+        if not liked:
+            continue
+        print(random.choice(ingredients))
+    return order
+
+
+def main():
+    preferences = customer_preferences()
+    order = make_order(preferences)
+    print("Here is your order:")
+    for ingredient in order:
+        print("A {}".format(ingredient))
+        
+if __name__ == "__main__":
+    main()
